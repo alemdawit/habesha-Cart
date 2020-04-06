22dc07de0c7dcc475f228a8bc870e8d62d944947
alemdawit <alemdawit.star@gmail.com>
2020-04-05 22:23:07 -0700

    alemdawit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBvBN0y75sPyLuVAUM4TZKTDjpY7zlfzoo",
+    authDomain: "myhabeshacart.firebaseapp.com",
+    databaseURL: "https://myhabeshacart.firebaseio.com",
+    projectId: "myhabeshacart",
+    storageBucket: "myhabeshacart.appspot.com",
+    messagingSenderId: "874067133591",
+    appId: "1:874067133591:web:c715b67e0444331c5855d2",
+    measurementId: "G-FLJVZ4H214"
+  }
 };
---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,10 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyBvBN0y75sPyLuVAUM4TZKTDjpY7zlfzoo",
+  authDomain: "myhabeshacart.firebaseapp.com",
+  databaseURL: "https://myhabeshacart.firebaseio.com",
+  projectId: "myhabeshacart",
+  storageBucket: "myhabeshacart.appspot.com",
+  messagingSenderId: "874067133591",
+  appId: "1:874067133591:web:c715b67e0444331c5855d2",
+  measurementId: "G-FLJVZ4H214"
+};
